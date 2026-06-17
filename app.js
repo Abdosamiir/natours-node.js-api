289a57865793d08e03df263830d62a49aaf96157
abdosamiir <barakatabdo82@gmail.com>
2026-06-17 16:51:03 +0300

    Implement POST endpoint for adding new tours and update tours data file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// middleware => modifies the incoming request data before it reaches the route handler
+app.use(express.json());
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -29,4 +32,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+  // res.send('POST request received!');
 });
