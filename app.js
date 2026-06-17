33c1859df5757be049efc0652f17c0d8bd188632
abdosamiir <barakatabdo82@gmail.com>
2026-06-17 17:05:57 +0300

    Add GET endpoint for retrieving tour details by ID and update tours data

--- app.js
+++ app.js
@@ -34,6 +34,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// response to url
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
