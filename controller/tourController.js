39b68bdca164bb32031c92ce7ff7e73bc352ac1b
abdosamiir <barakatabdo82@gmail.com>
2026-09-07 13:12:29 +0300

    Add ID validation in checkID middleware and improve error handling for tour routes

--- controller/tourController.js
+++ controller/tourController.js
@@ -4,6 +4,14 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(val);
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -18,8 +26,6 @@ exports.getTour = (req, res) => {
   // console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) res.status(404).json({ status: 'fail', message: 'Invalid ID' });
 
   res.status(200).json({
     status: 'success',
@@ -51,9 +57,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -63,9 +66,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
   res.status(204).json({
     status: 'success',
     data: null,
