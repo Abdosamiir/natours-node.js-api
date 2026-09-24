52207d7e9f17b085d7e416f66a41f07d4ad16aaf
abdosamiir <barakatabdo82@gmail.com>
2026-09-24 12:59:20 +0300

    Refactor tour controller: remove unused code and implement async/await for update and delete methods

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,18 +1,6 @@
 const fs = require('fs');
 const Tour = require('../models/tours/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(val);
-//   if (req.params.id * 1 > tours.length)
-//     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-
-//   next();
-// };
-
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price)
     return res.status(400).json({
@@ -75,18 +63,38 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
